c11d79dea8e0589091072a3e5d75070bff8c6fc0
odeane_croney <odeane_croney@yahoo.com>
2020-05-28 00:49:59 +0200

    creating calculation

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -1,6 +1,6 @@
 const calculateBMI = () => {
-    let weight = document.getElementById('weight')
-    let weight = document.getElementById('height')
+    let weightInput = document.getElementById('weight')
+    let heightInput = document.getElementById('height')
     let results = calculateMetricBMI(weightInput.value, heightInput.value)
     let displayResultsElement = document.getElementById('results')
     displayResultsElement.innerHTML = `<h1> Your BMI value is: ${results}</h1>`
